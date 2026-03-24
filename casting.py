9fcf7009f80925eebb24368b688f78989d706609
inikis <dinovelley@gmail.com>
2026-03-23 21:56:41 -0300

    intento 2

--- casting.py
+++ casting.py
@@ -1,5 +1,5 @@
 def casting():
-    """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
+    """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total"""
     precio=int(input("ingrese un precio: "))
     descuento=float(input("ingrese un descuento (valido con coma): "))
     cant=int(input("ingrese una cantidad: "))
